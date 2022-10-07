02ac7d9169cce065365399bdd2283c78e5ee9510
Emil Frisk <wallaton100@gmail.com>
2022-10-07 20:07:40 +0300

    Added DBURI

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const authRoutes = require('./routes/auth')
-const blogRoutes = require('./routes/blog')
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+const blogRoutes = require("./routes/blog");
+const cors = require("cors");
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors({
-    origin: '*'
-}))
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-const PORT = process.env.PORT || 5000
-const dbURI =  `mongodb+srv://eemeli123:${process.env.DB_PASSWORD}@cluster0.3ujv5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const PORT = process.env.PORT || 5000;
+const dbURI = process.env.MONGO_DB_URI;
 
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((result) => app.listen(PORT, () => console.log('Connection to DB successfull, listening at port 5000....')))
-    .catch(err => console.log(err))
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) =>
+    app.listen(PORT, () =>
+      console.log("Connection to DB successfull, listening at port 5000....")
+    )
+  )
+  .catch((err) => console.log(err));
 
-app.use(authRoutes)
-app.use(blogRoutes)
-
+app.use(authRoutes);
+app.use(blogRoutes);
